5c7f5dd1275fe4c8a669b71fc60d65c158cfb069
arif matubber <am@hoxro.com>
2025-09-26 23:47:12 +0600

    s

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
-import { render } from "react-dom";
-import { AppRouter } from './AppRouter';
-import './index.css';
-render(<AppRouter />, document.getElementById("root"));
+// import { render } from "react-dom";
+// import { AppRouter } from './AppRouter';
+// import './index.css';
+// render(<AppRouter />, document.getElementById("root"));
+import { inject } from '@vercel/analytics';
+
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { AppRouter } from './AppRouter'
+inject()
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<AppRouter />)
+
 
 // import { createRoot } from 'react-dom/client';
 // import './index.css'
